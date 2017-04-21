dae129bb346d1e9856eba76d80066acec80d8f6d
evangelista94 <echicheko@css.edu>
2017-04-21 11:34:02 -0500

    Participation 13 with Anja

--- app/src/main/java/css/cis3334/fishlocatorfirebase/AddFishActivity.java
+++ app/src/main/java/css/cis3334/fishlocatorfirebase/AddFishActivity.java
@@ -1,8 +1,12 @@
 package css.cis3334.fishlocatorfirebase;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +19,7 @@ public class AddFishActivity extends AppCompatActivity {
 
     Button buttonSave;
     EditText editTextSpecies, editTextWeight, editTextDate;
-    Double lattitude, longiture;
+    Double lattitude, longitude;
     FishFirebaseData fishDataSource;
 
     @Override
@@ -32,27 +36,45 @@ public class AddFishActivity extends AppCompatActivity {
         fishDataSource.open();
 
         // get the current location of the phone
-//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        Criteria criteria = new Criteria();
-//        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-//        lattitude = location.getLatitude();
-//        longiture = location.getLongitude();
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        } else {
 
-        // set up the button listener
-        buttonSave = (Button) findViewById(R.id.buttonSave);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                // Add the fish to the database
-                String species = editTextSpecies.getText().toString();
-                String weight = editTextWeight.getText().toString();
-                String dateCaught = editTextDate.getText().toString();
-                fishDataSource.createFish(species, weight, dateCaught);
-                //fishDataSource.createFish(species, weight, dateCaught, lattitude.toString(), longiture.toString());
-                Intent mainActIntent = new Intent(view.getContext(), MainActivity.class);
-                finish();
-                startActivity(mainActIntent);
+            Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+            if (location != null) {
+                lattitude = location.getLatitude();
+                longitude = location.getLongitude();
+            } else {
+                lattitude = 0.0;
+                longitude = 0.0;
             }
-        });
+        }
 
+            // set up the button listener
+            buttonSave = (Button) findViewById(R.id.buttonSave);
+            buttonSave.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    // Add the fish to the database
+                    String species = editTextSpecies.getText().toString();
+                    String weight = editTextWeight.getText().toString();
+                    String dateCaught = editTextDate.getText().toString();
+                    //fishDataSource.createFish(species, weight, dateCaught);
+                    fishDataSource.createFish(species, weight, dateCaught, lattitude.toString(), longitude.toString());
+                    Intent mainActIntent = new Intent(view.getContext(), MainActivity.class);
+                    finish();
+                    startActivity(mainActIntent);
+                }
+            });
+
+        }
     }
-}
+
